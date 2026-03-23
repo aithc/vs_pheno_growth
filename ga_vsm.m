5888bf2f78e331a47a231b9c8bce285533b9886f
today zhang <759585702@qq.com>
2026-03-23 18:12:44 +0100

    Clean up optimization options in ga_vsm.m
    Removed options for genetic algorithm and particle swarm optimization.

--- ga_vsm.m
+++ ga_vsm.m
@@ -16,14 +16,6 @@ nonlcon = [];
 fitness = @vsm_fit;
 
 nvars = 17; 
-%options = optimoptions('ga','PlotFcn',@gaplotbestf);
-
 [x_best,fval,flag,output] = ga(fitness,nvars,A,b,Aeq,beq,par_low,par_high, nonlcon, intcon);
 
 disp('3 over')
-%options = optimoptions('particleswarm','MinNeighborsFraction',1,'PlotFcn',@pswplotbestf);
-%options.SwarmSize = 400;
-%options.HybridFcn = @fmincon;
-
-%[x_best,fval,flag,output] = particleswarm(fitness,nvars,par_low,par_high,options);
-
