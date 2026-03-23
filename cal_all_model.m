0a3758e09755e219e6f1c29de96cd015533afb14
today zhang <759585702@qq.com>
2026-03-23 18:07:36 +0100

    Refactor file paths and clean up comments

--- cal_all_model.m
+++ cal_all_model.m
@@ -1,10 +1,8 @@
-% 计算所有 显著的点的  原始模型+原始数据  原始模型+去趋势数据  不考虑物候+原始数据 的结果
 
 %%
 clear; close all; clc;  % clear the workspace
 
-%% 读取所有的点的信息
-site_infos = readtable('D:/matlab_file/vs_model_for_spr_try/to_linux/tr_info_use_sig_matlab.csv');
+site_infos = readtable('tr_info_use_sig_matlab.csv');
 %length(info)
 
 %% calculate for all sites by loop
@@ -13,9 +11,8 @@ for i = 1:height(site_infos)
     lat_n = site_infos.('latitude')(i);
     lon_n = site_infos.('longitude')(i);
 
-    % 读取优化后的参数
     disp(sites_name);
-    fitted_file = ['D:/matlab_file/vs_model_for_spr_try/fitted_vsmodel/output_',sites_name{1},'_',int2str(site_infos.used(i)),'.mat'];
+    fitted_file = ['/fitted_vsmodel/output_',sites_name{1},'_',int2str(site_infos.used(i)),'.mat'];
     load(fitted_file);
     tf_1 = output.parameters(1); % minimum temperature (C) for growth
     tf_2 = output.parameters(2);  % growth rate is max in the range T2-T3 (lower optimal temperature, C)
@@ -58,8 +55,6 @@ for i = 1:height(site_infos)
     
     
 
-    % 读取气候数据
-    % 原始的
     dataset = data_prepare(sites_name{1},1960,2015,lat_n,lon_n);  % get data
 
     T  = dataset.T;
@@ -70,7 +65,7 @@ for i = 1:height(site_infos)
     P = dataset.P;
     phi = dataset.lat;
     
-    % 去趋势的
+
     dataset_d = data_prepare_de(sites_name{1},1960,2015,lat_n,lon_n);  % get data
 
     T_d  = dataset_d.T;
@@ -85,29 +80,27 @@ for i = 1:height(site_infos)
     eyear = dataset.eyear;
     chronology = dataset.std;
     
-    % 计算原始模型+原始气候数据
+
     tic % based on testing, this loop may take anywhere from 1 to 20 seconds, depending on your system
     output = vsm_me(T,P,phi,syear,eyear,tf_1,tf_2,tf_3,tf_4,wf_1,wf_2,wf_3,wf_4,SNo,Wo, ... ,
         rootd,rated,a_1,a_2,Tg,SNr,K_9);
     toc %  
     
-    % 计算原始模型+去趋势气候
     tic % based on testing, this loop may take anywhere from 1 to 20 seconds, depending on your system
     output_de = vsm_me(T_d,P_d,phi,syear,eyear,tf_1,tf_2,tf_3,tf_4,wf_1,wf_2,wf_3,wf_4,SNo,Wo, ... ,
         rootd,rated,a_1,a_2,Tg,SNr,K_9);
     toc %
 
-    % 计算不考虑物候的模型 + 原始气候数据
     tic % based on testing, this loop may take anywhere from 1 to 20 seconds, depending on your system
     output_nopheno = vsm_no_pheno(T,P,phi,syear,eyear,tf_1,tf_2,tf_3,tf_4,wf_1,wf_2,wf_3,wf_4,SNo,Wo, ... ,
     rootd,rated,a_1,a_2,SNr,output_de.fday);
     toc %  
 
-    % 导出
+
     
-    output_file_raw = ['D:/matlab_file/vs_model_for_spr_try/final_fitted/result_raw',sites_name{1},'.mat'];
-    output_file_de = ['D:/matlab_file/vs_model_for_spr_try/final_fitted/result_de',sites_name{1},'.mat'];
-    output_file_np = ['D:/matlab_file/vs_model_for_spr_try/final_fitted/result_np',sites_name{1},'.mat'];
+    output_file_raw = ['/final_fitted/result_raw',sites_name{1},'.mat'];
+    output_file_de = ['/final_fitted/result_de',sites_name{1},'.mat'];
+    output_file_np = ['/final_fitted/result_np',sites_name{1},'.mat'];
     
     save(output_file_raw,'output');
     save(output_file_de,'output_de');
